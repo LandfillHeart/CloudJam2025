101215aafae8885b1decd8420dc3dd8b7f54bdaf
LandfillHeart <kimbleredward@gmail.com>
2025-11-15 18:07:40 +0100

    Aggiustato clamp movimento, jump
    - Il clamp direzione di movimento era erroneamente 0-1 invece che -1 a 1
    - Aggiunta di Jump che permette il salto solo se si sta toccando il suolo
    - Aumento graduale della velocità quando in caduta, visto che la gravità di Unity ha un effetto "floaty"

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,29 +4,37 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 	[SerializeField] private float walkSpeed;
+	[SerializeField] private float jumpStrength;
 
 	private Rigidbody2D rb;
 
 	private float directionHorizontal;
+	private LayerMask groundLayer;
 
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		groundLayer = LayerMask.GetMask("Ground");
 	}
 
 	private void FixedUpdate()
 	{
 		// move position will override the vertical movement from gravity
 		rb.AddForceX(walkSpeed * directionHorizontal, ForceMode2D.Impulse);
-		if(Mathf.Abs(rb.linearVelocityX) > walkSpeed)
+		if (Mathf.Abs(rb.linearVelocityX) > walkSpeed)
 		{
 			rb.AddForceX((Mathf.Abs(rb.linearVelocityX) - walkSpeed) * -directionHorizontal, ForceMode2D.Impulse);
+		}
+
+		if (rb.linearVelocityY < 0f)
+		{
+			rb.AddForceY(Physics2D.gravity.y, ForceMode2D.Force);
 		}
 	}
 
 	public void Move(float direction)
 	{
-		direction = Mathf.Clamp01(direction);
+		direction = Mathf.Clamp(direction, -1, 1);
 		this.directionHorizontal = direction;
 		if (Mathf.Approximately(direction, 0f)) 
 		{
@@ -37,7 +45,9 @@ public class Movement : MonoBehaviour
 
 	public void Jump()
 	{
-
+		Debug.DrawRay(transform.position, Vector2.down * 1.15f, Color.yellow, 3f);
+		if (!Physics2D.Raycast(transform.position, Vector2.down, 1.15f, groundLayer)) { return; }
+		rb.AddForceY(jumpStrength, ForceMode2D.Impulse);
 	}
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,16 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private Movement playerMovement;
 
 	private InputAction moveAction;
+	private InputAction jumpAction;
 
 	private void Start()
 	{
 		moveAction = playerControlsMap.FindAction("Move");
+		jumpAction = playerControlsMap.FindAction("Jump");
 
 		moveAction.performed += (ctx) => playerMovement.Move(ctx.ReadValue<Vector2>().x);
 		moveAction.canceled += (ctx) => playerMovement.Move(0);
-	}
 
+		jumpAction.started += (ctx) => playerMovement.Jump();
+	}
 }
