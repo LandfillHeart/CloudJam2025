0c3c8469cec856502474d66d7d0043e75a242c6a
Rela45 <alex.imbriani00@gmail.com>
2025-11-19 18:26:05 +0100

    Adjust camera follow behavior and update scene object positions

--- Assets/Scripts/CameraOnPlayer.cs
+++ Assets/Scripts/CameraOnPlayer.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class CameraOnPlayer : MonoBehaviour
 {
     [SerializeField] private Transform _target;
-    public Vector3 offset = new Vector3(0.0f, 1.0f, -10.0f);
-    [Range(0.0f, 1.0f)]public float smoothness = 0.5f;
+    public Vector3 offset = new Vector3(0.0f, 10.0f, -10.0f);
+    [Range(1.0f, 0.0f)]public float smoothness = 0.5f;
 
     private Vector3 _velocity;
-    
+
+    private float fixedY; 
+
+    void Start()
+    {
+        // Salviamo la Y iniziale della camera
+        fixedY = transform.position.y;
+    }
+
     void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, 
-        _target.position + offset, 
-        ref _velocity,
-        smoothness
-        );
+        Vector3 desiredPos = _target.position + offset;
 
+        // Mantieni la Y fissa
+        desiredPos.y = fixedY;
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            desiredPos,
+            ref _velocity,
+            smoothness
+        );
     }
 }
